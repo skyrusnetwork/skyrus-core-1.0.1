df2fd0bfb400fc1e3447409a2c5e6d103f357aca
VangYangPao <vangyangpao@gmail.com>
2018-11-01 10:27:28 +0700

    processing

--- contrib/spendfrom/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='SKXspendfrom',
-      version='1.0',
-      description='Command-line utility for skyrus "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@skyrusfoundation.org',
-      requires=['jsonrpc'],
-      scripts=['spendfrom.py'],
-      )
